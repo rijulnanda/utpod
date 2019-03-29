aa1de574dd2730c22ffdddb36e285f78f12dffcd
Andrew Annestrand <aannestrand98@gmail.com>
2019-03-29 13:33:56 -0500

    mr. poopy butthole

---
+++ Song.h
@@ -0,0 +1,51 @@
+#ifndef SONG_H
+#define SONG_H
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+class Song {
+
+    private:
+        //song size
+        int size;
+
+        //song title
+        string title;
+
+        //song artist
+        string artist;
+
+    public:
+        //Default Constructor
+        Song();
+
+        //Constructor with artist/title/size
+        Song(string artist, string title, int size);
+        
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
