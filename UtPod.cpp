04e1e6baba82c7c9d459480c84db9bfffd5e0ca4
Andrew Annestrand <aannestrand98@gmail.com>
2019-03-29 15:35:38 -0500

    broke ass code

--- UtPod.cpp
+++ UtPod.cpp
@@ -1,145 +1,153 @@
-//DOCUMENT HERE
-
-#ifndef UTPOD_H
-#define UTPOD_H
+#include <cstdlib>
+#include <iostream>
 #include "Song.h"
+#include "UtPod.h"
 
-//UtPod class declaration
-class UtPod
-{
-   private:
-      static const int MAX_MEMORY = 512;
-      static const int SUCCESS = 0;
-      static const int NO_MEMORY = -1;
-      static const int NOT_FOUND = -2;
+using namespace std;
       
-      struct SongNode
-      {
-         Song s;
-         SongNode *next;
-      };
-      
-      SongNode *songs;  //the head pointer
-      
-      int podMemSize;  //the total amount of memory available on the UtPod
-   
-   public:
-      //Default constructor
-      //set the memor,y size to MAX_MEMORY
-      UtPod();
+    //Default constructor
+    //set the memory size to MAX_MEMORY
+    UtPod::UtPod() {
+        podMemSize = MAX_MEMORY;
+        songs = NULL;
+    }
 
-      //Constructor with size parameter
-      //The user of the class will pass in a size.
-      //If the size is greater than MAX_MEMORY or less than or equal to 0,
-      //set the size to MAX_MEMORY.
-      UtPod(int size);
+    //Constructor with size parameter
+    //The user of the class will pass in a size.
+    //If the size is greater than MAX_MEMORY or less than or equal to 0,
+    //set the size to MAX_MEMORY.
+    UtPod::UtPod(int size) {
+        if (size > MAX_MEMORY || size <= 0)
+            podMemSize = MAX_MEMORY;
+        else 
+            podMemSize = size;
 
-      /* FUNCTION - int addSong
-       * attempts to add a new song to the UtPod
-           o returns a 0 if successful
-           o returns -1 if not enough memory to add the song
+        songs = NULL;
+    }
 
-       precondition: s is a valid Song
+    /* FUNCTION - int addSong
+    * attempts to add a new song to the UtPod
+        o returns a 0 if successful
+        o returns -1 if not enough memory to add the song
 
-       input parms -
+    precondition: s is a valid Song
 
-       output parms -
-      */
+    input parms -
 
-      int addSong(Song const &s);
+    output parms -
+    */
 
+    int UtPod::addSong(Song const &s) {
+        SongNode* node = new SongNode;
+        node->s = s;
 
-      /* FUNCTION - int removeSong
-       * attempts to remove a song from the UtPod
-       * removes the first instance of a song that is in the the UtPod multiple times
-           o returns 0 if successful
-           o returns -1 if nothing is removed
+        node->next = songs;
+        songs = node;
 
-
-         input parms -
-
-         output parms -
-      */
-
-      int removeSong(Song const &s);
+        return 0;
+    }
 
 
-      /* FUNCTION - void shuffle
-       *  shuffles the songs into random order
-          o will do nothing if there are less than two songs in the current list
-
-         input parms -
-
-         output parms -
-      */
-
-      void shuffle();
+    /* FUNCTION - int removeSong
+    * attempts to remove a song from the UtPod
+    * removes the first instance of a song that is in the the UtPod multiple times
+        o returns 0 if successful
+        o returns -1 if nothing is removed
 
 
-      /* FUNCTION - void showSongList
-       * prints the current list of songs in order from first to last to standard output
-       * format - Title, Artist, size in MB (one song per line)
+        input parms -
 
-         input parms -
+        output parms -
+    */
 
-         output parms -
-      */
+    int UtPod::removeSong(Song const &s) {
 
-      void showSongList();
+    }
 
 
-      /* FUNCTION - void sortSongList
-       *  sorts the songs in ascending order
-          o will do nothing if there are less than two songs in the current list
+    /* FUNCTION - void shuffle
+    *  shuffles the songs into random order
+        o will do nothing if there are less than two songs in the current list
 
-         input parms -
+        input parms -
 
-         output parms -
-      */
+        output parms -
+    */
 
-      void sortSongList();
+    void UtPod::shuffle() {
 
-
-      /* FUNCTION - void clearMemory
-       * clears all the songs from memory
-
-         input parms -
-
-         output parms -
-      */
-      void clearMemory();
+    }
 
 
-      /* FUNCTION - int getTotalMemory
-       *  returns the total amount of memory in the UtPod
-          o will do nothing if there are less than two songs in the current list
+    /* FUNCTION - void showSongList
+    * prints the current list of songs in order from first to last to standard output
+    * format - Title, Artist, size in MB (one song per line)
 
-         input parms -
+        input parms -
 
-         output parms -
-      */
+        output parms -
+    */
 
-      int getTotalMemory() {
-         return memSize;
-      }
+    void UtPod::showSongList() {
 
-
-
-      /* FUNCTION - int getRemainingMemory
-       *  returns the amount of memory available for adding new songs
-
-         input parms -
-
-         output parms -
-      */
-
-      int getRemainingMemory();
+    }
 
 
-      ~UtPod();
- 
-};
+    /* FUNCTION - void sortSongList
+    *  sorts the songs in ascending order
+        o will do nothing if there are less than two songs in the current list
+
+        input parms -
+
+        output parms -
+    */
+
+    void UtPod::sortSongList() {
+
+    }
+
+
+    /* FUNCTION - void clearMemory
+    * clears all the songs from memory
+
+        input parms -
+
+        output parms -
+    */
+    void UtPod::clearMemory() {
+
+    }
+
+
+    /* FUNCTION - int getTotalMemory
+    *  returns the total amount of memory in the UtPod
+        o will do nothing if there are less than two songs in the current list
+
+        input parms -
+
+        output parms -
+    */
+
+    int UtPod::getTotalMemory() {
+        return podMemSize;
+    }
 
 
 
-#endif
+    /* FUNCTION - int getRemainingMemory
+    *  returns the amount of memory available for adding new songs
+
+        input parms -
+
+        output parms -
+    */
+
+    int UtPod::getRemainingMemory() {
+
+    }
+
+
+    UtPod::~UtPod() {
+
+    }
+
